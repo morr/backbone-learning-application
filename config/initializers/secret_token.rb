d75615607d04295b5feb9e620cd0b3539fac6d80
Andrey Sidorov <takandar@gmail.com>
2012-05-29 14:30:10 +0400

    голое rails приложение

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneBem::Application.config.secret_token = '5a8c4828fb8e0190aaa2f4f827f0ee732f37b03f2dbe13555079c61526bf0d59c3a17782c2ca3c4ccc1de33cbc9c675b9ebcdce7fae0b2507bd07a8e9c21a7a8'
