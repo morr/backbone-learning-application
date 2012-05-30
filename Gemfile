a76a1cf6e1a64832af7a327f02c4536b49f71d17
Andrey Sidorov <takandar@gmail.com>
2012-05-30 21:02:17 +0400

    запилил основной шаблон

--- Gemfile
+++ Gemfile
@@ -22,8 +22,8 @@ group :assets do
 end
 
 gem 'jquery-rails'
-
 gem 'backbone-on-rails'
+gem 'haml'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/wizard_controller.rb
+++ app/controllers/wizard_controller.rb
@@ -1,4 +1,21 @@
+# encoding: utf-8
+
 class WizardController < ApplicationController
+  helper_method :steps
+  Steps = [
+    { name: 'Данные перелёта', template: 'wizard/route' },
+    { name: 'Авторизация', template: 'wizard/auth' },
+    { name: 'Договор', template: 'wizard/agreement' },
+    { name: 'Паспорта', template: 'wizard/people' },
+    { name: 'Оплата', template: 'wizard/payment' },
+    { name: 'Билеты', template: 'wizard/tickets' }
+  ]
+
   def show
+  end
+
+private
+  def steps
+    Steps
   end
 end
